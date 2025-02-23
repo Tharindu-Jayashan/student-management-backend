9478a9df4330398f49f4ed243cc416a8d6a82ef9
Tharindu Jayashan <tharindujayashan421@gmail.com>
2025-02-23 14:54:17 +0530

    established a connection to MongoDB using  Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-let app  = express();
+dotenv.config();
+
+const app  = express();
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
+})
+
+const mongoUrl = process.env.MONGO_URL
+mongoose.connect(mongoUrl)
+
+const connection = mongoose.connection;
+
+connection.once("open", ()=> {
+    console.log("MongoDB database connection established successfully")
 })
 
